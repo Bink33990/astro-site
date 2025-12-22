433f1f9bb4267674039f1476336f6f3436bd5c2c
Bink <brendonkillingsworth@gmail.com>
2025-12-22 09:32:38 -0500

    remove image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
-import { imageServiceConfig } from "./src/services/image-service";
- 
-export default defineConfig({
-    // ....
-  image: {
-    service: imageServiceConfig(),
-  },
-}
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
